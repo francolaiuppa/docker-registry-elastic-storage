f6ba127a5193c8452a05494493405cc1f81a25f8
Franco Laiuppa <franco.laiuppa@gmail.com>
2016-09-10 13:26:01 +0200

    Added some whitespace for better reading

--- setup.sh
+++ setup.sh
@@ -43,48 +43,63 @@ dockerssh systemctl stop docker
 
 echo "Formatting elastic block storage"
 # format volume as Digital Ocean recommends, even though we're going to drop it later
-
-### DELETE THIS LINE BELOW BEFORE PUBLISHING
-#dockerssh umount $ELASTIC_BLOCK_STORAGE_PATH
-
 dockerssh mkfs.ext4 -F $ELASTIC_BLOCK_STORAGE_PATH
+
 echo "Creating mountpoint"
 dockerssh mkdir -p $MNT_DOCKER_VOLUME
 dockerssh mount -o discard,defaults $ELASTIC_BLOCK_STORAGE_PATH $MNT_DOCKER_VOLUME
+
 echo "Adding to fstab"
 dockerssh "echo -n \n$ELASTIC_BLOCK_STORAGE_PATH $MNT_DOCKER_VOLUME ext4 defaults,nofail,discard 0 0 >> /etc/fstab"
+
 echo "Unmount to prepare for lvm"
 dockerssh umount /dev/sda
+
 echo "Installing lvm2 packages"
 dockerssh yum install -y lvm2*
+
 echo "Create LVM Physical Volume"
 dockerssh pvcreate -f /dev/sda
+
 echo "Create LVM Virtual Group on LVM Physical Volume"
 dockerssh vgcreate docker /dev/sda
+
 echo "Create LVM Logical Volume for docker graph data"
 dockerssh lvcreate --wipesignatures y -n thinpool docker -l 95%VG
+
 echo "Create LVM Logical Volume for docker graph metadata"
 dockerssh lvcreate --wipesignatures y -n thinpoolmeta docker -l 1%VG
+
 echo "Formatting lvm volumes to be used as a thinpool"
 dockerssh lvconvert -y --zero n -c 512K --thinpool docker/thinpool --poolmetadata docker/thinpoolmeta
+
 echo "Enabling docker lvm thin pool"
 dockerssh curl -o /etc/lvm/profile/docker-thinpool.profile https://raw.githubusercontent.com/francolaiuppa/docker-registry-elastic-storage/master/docker-thinpool.profile
+
 echo "Changing docker-thinpool to be a metadataprofile type"
 dockerssh lvchange --metadataprofile docker-thinpool docker/thinpool
+
 echo "Configure Docker daemon to run using the new docker-thinpool storage"
 dockerssh curl -o /etc/docker/daemon.json https://raw.githubusercontent.com/francolaiuppa/docker-registry-elastic-storage/master/daemon.json
+
 echo "Cleaning old docker files"
 dockerssh rm -rf /var/lib/docker
+
 echo "Reloading systemd unit file"
 dockerssh systemctl daemon-reload
+
 echo "Starting Docker Service"
 dockerssh systemctl start docker
+
 echo "Preparing data folders for Registry"
 dockerssh mkdir -p /var/www/registry/data
 dockerssh chmod 777 /var/www/registry/data
+
 echo "Run Registry. TODO Add authentication"
 dockerssh docker run -d --name registry -p 5000:5000 -v $(pwd)/data:/tmp/registry-dev registry:2.5.0
+
 echo "Run Docker Registry Frontend at port 8080"
 dockerssh docker run -d -e ENV_DOCKER_REGISTRY_HOST=$DROPLET_IP -e ENV_DOCKER_REGISTRY_PORT=5000 -p 8080:80 konradkleine/docker-registry-frontend:v2
+
 # the message you were waiting for
 echo "All done, please visit http://$DROPLET_IP:8080 in order to view your shiny new Private Docker Registry"
